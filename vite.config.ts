011fd77713eab65d2b0f774935a4c2e91d02ed4a
Muhammad Sholeh <avsholeh@gmail.com>
2025-10-30 00:34:14 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 8000,
+  },
   plugins: [react(), tsconfigPaths()],
-})
+});
